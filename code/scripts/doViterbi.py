da6db6564cc1c850e132c64005f727b0bc4a82af
Joaquin Rapela <joacorapela@gmail.com>
2026-04-24 16:32:45 +0100

    A few relevant changes (e.g., computeLatentsMeansWithEquispacedTrialsTimes)

--- code/scripts/doViterbi.py
+++ code/scripts/doViterbi.py
@@ -16,76 +16,100 @@ def main(argv):
     parser = argparse.ArgumentParser()
     parser.add_argument("--train_est_res_number",
                         help="model estimation result number used for learning the HMM model",
-                        type=int, default=54368807)
+                        type=int,
+                        default=556223)
+                        # default=54368807)
     parser.add_argument("--test_est_res_number",
                         help="model estimation result number used for testing the HMM model",
-                        type=int, default=54368807)
+                        type=int,
+                        default=556223)
+                        # type=int, default=20263319)
+                        # type=int, default=99226606)
+                        # type=int, default=42833278)
+                        # type=int, default=99749566)
+                        # type=int, default=88072043)
+                        # type=int, default=92418550)
+                        # type=int, default=54368807)
     parser.add_argument("--inferred",
                         help="variables were inferred and not estimated",
                         action="store_true")
     parser.add_argument("--model_filename_pattern",
                         help="model filename pattern", type=str,
                         default="../../results/EJT178_implant1/recording6_29-03-2022/{:08d}_estimatedModel.pickle")
+                        # default="../../results/EJT178_implant1/recording6_29-03-2022/{:08d}_inferredModel.pickle")
     parser.add_argument("--hmm_params_filename_pattern", type=str,
                         help="hmm parameters filename pattern",
-                        default="../../results/EJT178_implant1/recording6_29-03-2022/{:08d}_hmm_params.npz")
+                        default="../../results/EJT178_implant1/recording6_29-03-2022/{:08d}_hmm_params.{:s}")
     parser.add_argument("--most_prob_states_seq_filename_pattern", type=str,
                         help="filtering_res filename pattern",
-                        default="../../results/EJT178_implant1/recording6_29-03-2022/train{:08d}_test{:08d}_hmm_most_prob_state_seq.npz")
+                        default="../../results/EJT178_implant1/recording6_29-03-2022/train{:08d}_test{:08d}_hmm_most_prob_state_seq.pickle")
     args = parser.parse_args()
 
     train_est_res_number = args.train_est_res_number
     test_est_res_number = args.test_est_res_number
     inferred = args.inferred
     test_model_filename = args.model_filename_pattern.format(test_est_res_number)
+    hmm_params_metadata_filename = args.hmm_params_filename_pattern.format(
+        train_est_res_number, "ini")
     hmm_params_filename = args.hmm_params_filename_pattern.format(
-        train_est_res_number)
+        train_est_res_number, "pickle")
     most_prob_states_seq_filename = args.most_prob_states_seq_filename_pattern.format(
         train_est_res_number, test_est_res_number)
 
+    metadata_config = configparser.ConfigParser()
+    metadata_config.read(hmm_params_metadata_filename)
+    latents_sample_rate = int(metadata_config["estimation_params"]["latents_sample_rate"])
 
     with open(test_model_filename, "rb") as f:
         test_est_results = pickle.load(f)
 
     kernels_types = test_est_results["kernels_types"]
-
-    leg_quad_points = test_est_results["estimation_params"]["ell_calculation_params"]["leg_quad_points"]
-    reg_param = test_est_results["estimation_params"]["optim_params"]["prior_cov_reg_param"]
+    # reg_param = test_est_results["estimation_params"]["optim_params"]["prior_cov_reg_param"]
+    reg_param = 1e-5
     estimated_params = test_est_results["estimated_params"]
     if inferred:
         fixed_params = test_est_results["fixed_params"]
+        # fixed_params = test_est_results["fixed_params"][0]
+    trials_start_times = test_est_results["trials_start_times"]
+    trials_end_times = test_est_results["trials_end_times"]
 
     vMean = estimated_params["variational_mean"]
-    vChol = estimated_params["variational_chol_vecs"]
     if inferred:
         kernels_params = fixed_params["kernels_params"]
+        C = fixed_params["C"]
     else:
         kernels_params = estimated_params["kernels_params"]
+        C = estimated_params["C"]
     ind_points_locs = estimated_params["ind_points_locs"]
 
-    # extract latents means and varances and estimated C
-    l_means, _ = svGPFA.utils.statsUtils.computeLatents(
-        vMean=vMean, vChol=vChol, kernels_params=kernels_params,
+    trials_times = svGPFA.utils.miscUtils.getEquispacedTrialsTimes(
+        trials_start_times=trials_start_times,
+        trials_end_times=trials_end_times,
+        sample_rate=latents_sample_rate)
+
+    # extract latents means
+    l_means = svGPFA.utils.statsUtils.computeLatentsMeansWithEquispacedTrialsTimes(
+        vMean=vMean, kernels_params=kernels_params,
         ind_points_locs=ind_points_locs, kernels_types=kernels_types,
-        leg_quad_points=leg_quad_points, reg_param=reg_param)
+        trials_times=trials_times, reg_param=reg_param)
 
-    # trials_times \in n_trials \times n_quad \times 1
-    trials_times = jnp.asarray(leg_quad_points)
-    # l_means \in n_latents \times n_quad \times n_trials
-    l_means = np.transpose(jnp.asarray(l_means), (0, 2, 1))
+    l_means = [l_mean.transpose(1, 0) for l_mean in l_means]
+    ol_means = svGPFA.utils.miscUtils.orthogonalizeLatentsMeans(
+        latents_means=l_means, C=C)
 
-    load_res = np.load(hmm_params_filename)
-    state_labels = load_res["states_labels"]
-    Pi = load_res["Pi"]
-    A = load_res["A"]
-    means = load_res["means"]
-    covs = load_res["covs"]
+    with open(hmm_params_filename, "rb") as f:
+        hmm_params = pickle.load(f)
+    state_labels = hmm_params["states_labels"]
+    Pi = hmm_params["Pi"]
+    A = hmm_params["A"]
+    means = hmm_params["means"]
+    covs = hmm_params["covs"]
 
-    T = l_means.shape[2]
+    T = len(l_means)
     Pi_for_trials = np.tile(Pi[:, np.newaxis], (1, T))
 
     start_time = time.time()
-    p = hmmUtils.getGaussianProbabilitiesEpoched(x=l_means, means=means,
+    p = hmmUtils.getGaussianProbabilitiesEpoched(x=ol_means, means=means,
                                                  covs=covs)
     elapsed_time = time.time() - start_time
     print(f"getGaussianProbabilitiesEpoched elapsed time={elapsed_time}")
@@ -95,8 +119,10 @@ def main(argv):
     elapsed_time = time.time() - start_time
     print(f"viterbiEpoched elapsed time={elapsed_time}")
 
-
-    np.savez(most_prob_states_seq_filename, state_labels=state_labels, trials_times=trials_times, most_prob_states_seq=most_prob_states_seq)
+    results = dict(state_labels=state_labels, trials_times=trials_times,
+                   most_prob_states_seq=most_prob_states_seq)
+    with open(most_prob_states_seq_filename, "wb") as f:
+        pickle.dump(results, f)
 
     print(f"Most probable states saved to {most_prob_states_seq_filename}")
 
